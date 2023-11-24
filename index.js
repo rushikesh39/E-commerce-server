1f8d353c25107d4b7eddc9d232e63067f126d52d
rushi <rushikeshthange2001@gmail.com>
2023-11-24 07:50:40 +0530

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const stripe=require("stripe")("sk_test_51OFa7xSI55uByng4ykFMInzewhC3lSJ2f9ElSCaYIgNjcOPZ50mpcmB7t9jf8QzLeUBcv3egMxRpmL3KlyX7hEfW00Y2yKHXqS")
 require('dotenv').config()
 
 const connectToDB=require('./db/connection')
@@ -17,6 +18,33 @@ app.use(cors());
 app.use(express.json());
 app.use("/product",productRoutes)
 app.use(userRoutes)
+
+app.post("/api/create-checkout-session",async(req,res)=>{
+  const {products}=req.body;
+  console.log(products)
+  
+const lineItems=products.map((product)=>({
+    price_data:{
+      currency:"inr",
+      product_data:{
+        name:product.product_name,
+      },
+      unit_amount:product.discounted_price*100,
+    },
+    quantity:product.quantity
+
+  }))
+
+  const session=await stripe.checkout.sessions.create({
+    payment_method_types:["card"],
+    line_items:lineItems,
+    mode:"payment",
+    success_url:"https://shopeswift.netlify.app/success",
+    cancel_url:"https://shopeswift.netlify.app/cancel",
+  })
+  res.send({id:session.id})
+})
+
 app.listen(5000,async()=>{
   try{
     await connectToDB()
@@ -27,26 +55,3 @@ app.listen(5000,async()=>{
   }
 })
 
-// async function dbconnect(){
-//     try{
-//         await connectToDB(process.env.URL)
-    
-//     }
-//     catch(e){
-//         console.log(e)
-//     }
-// }
-
-
-// async function startServer() {
-//     try {
-//     await dbconnect();
-//       app.use("/user",userRoutes)
-//       app.use('/product',productRoutes)
-//       app.listen(5000, () => console.log('Server is running on port 5000'));
-//     } catch (error) {
-//       console.error('Error starting the server:', error);
-//     }
-//   }
-  
-//   startServer(); 
